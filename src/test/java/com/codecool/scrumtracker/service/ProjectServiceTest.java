67054994b3b2cdd673baea34543ccf2c2d242cb2
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-07-26 16:12:31 +0200

    add, addNeStatusToProject test

--- src/test/java/com/codecool/scrumtracker/service/ProjectServiceTest.java
+++ src/test/java/com/codecool/scrumtracker/service/ProjectServiceTest.java
@@ -3,10 +3,14 @@ package com.codecool.scrumtracker.service;
 import com.codecool.scrumtracker.exception.exceptions.NotAuthorizedException;
 import com.codecool.scrumtracker.model.AppUser;
 import com.codecool.scrumtracker.model.Project;
+import com.codecool.scrumtracker.model.ScrumTable;
 import com.codecool.scrumtracker.model.Status;
 import com.codecool.scrumtracker.model.credentials.ProjectCredentials;
+import com.codecool.scrumtracker.model.credentials.StatusCredentials;
 import com.codecool.scrumtracker.model.credentials.UserCredentials;
 import com.codecool.scrumtracker.repository.ProjectRepository;
+import com.codecool.scrumtracker.repository.ScrumTableRepository;
+import com.codecool.scrumtracker.repository.StatusRepository;
 import com.codecool.scrumtracker.util.Util;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
@@ -21,6 +25,10 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.mockito.Mockito.*;
 
@@ -38,7 +46,14 @@ public class ProjectServiceTest {
     ProjectRepository projectRepository;
 
     @MockBean
+    StatusRepository statusRepository;
+
+    @MockBean
+    ScrumTableRepository scrumTableRepository;
+
+    @MockBean
     Util util;
+
 
     @Test
     public void testGetProjectById() throws NotAuthorizedException {
@@ -48,11 +63,49 @@ public class ProjectServiceTest {
                 .title("test project")
                 .build();
 
+
         when(util.projectAuthorization(any())).thenReturn(true);
 
         Mockito.when(projectRepository.findById(any())).thenReturn(java.util.Optional.ofNullable(testProject));
 
         Assertions.assertThat(projectService.getProjectById(any())).isEqualTo(testProject);
+    }
+
+    @Test
+    public void testAddNewStatusToProject() {
+
+        Status toDo = Status.builder()
+                .statusName("To Do")
+                .position(1)
+                .build();
+
+
+        Set<Status> testStatuses = new HashSet<>(Arrays.asList(toDo));
+
+        ScrumTable testTable = ScrumTable.builder()
+                .statuses(testStatuses)
+                .taskLimit(0)
+                .build();
+
+        Project testProject = Project.builder()
+                .archive(false)
+                .title("test project")
+                .table(testTable)
+                .build();
+
+        StatusCredentials testCredentials = StatusCredentials.builder()
+                .projectId(testProject.getId())
+                .statusName("test status")
+                .build();
+
+        Mockito.when(projectRepository.findById(any())).thenReturn(java.util.Optional.of(testProject));
+
+        assertThat(projectService.addNewStatusToProject(testCredentials)
+                .getStatuses()
+                .stream()
+                .mapToInt(Status::getPosition)
+                .max()
+                .getAsInt()).isEqualTo(2);
     }
 
 
